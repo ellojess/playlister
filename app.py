2f6291a59a5812586c23d656e694916e5311d92a
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-09-23 23:24:35 -0700

    add Playlist model

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ playlists = db.playlists
 app = Flask(__name__)
 
 # OUR MOCK ARRAY OF PROJECTS
-playlists = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
+# playlists = [
+#     { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+#     { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+# ]
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists)
+    return render_template('playlists_index.html', playlists=playlists.find())
 
 def index():
     """Return homepage."""
