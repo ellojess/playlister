f1e655d04f6994a79a7dfae133e3ebc2b89a5ee1
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-09-27 13:46:25 -0700

    Users can edit, update, and destroy playlists

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def index():
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
-    return render_template('playlists_new.html')
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -39,7 +39,25 @@ def playlists_show(playlist_id):
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
 
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    video_links = '\n'.join(playlist.get('videos'))
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
 
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 if __name__ == '__main__':
     app.run(debug=True)
