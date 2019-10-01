a7cc274b8cd44358a853a4baa504f1c21dc32001
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-10-01 09:36:15 -0700

    default database update

--- app.py
+++ app.py
@@ -4,9 +4,8 @@ from bson.objectid import ObjectId
 import os
 from datetime import datetime
 
-client = MongoClient()
-db = client.Playlister
-playlists = db.playlists
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+client = MongoClient(host=host)
 db = client.get_default_database()
 playlists = db.playlists
 comments = db.comments
